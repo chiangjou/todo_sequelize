d14c2b69e15d9fa7f910af08fa33b1f43ad9d670
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-07-21 18:15:25 +0800

    add login feature via passport

--- app.js
+++ app.js
@@ -2,17 +2,31 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
-const app = express()
-const PORT = 3000
+const session = require('express-session')
 
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
 
+const usePassport = require('./config/passport')
+const passport = require('passport')
+
+const app = express()
+const PORT = 3000
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
 
 // 設定路由
 // 首頁
@@ -39,9 +53,10 @@ app.get('/users/login', (req, res) => {
 })
 
 // 登入檢查
-app.post('users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 // 註冊頁面
 app.get('/users/register', (req, res) => {
