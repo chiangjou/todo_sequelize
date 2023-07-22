74b717379d01595a4ffaae9f95cead400b779b5c
chiangjou <tiffanyj.chiang@gmail.com>
2023-07-22 16:39:40 +0800

    add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,6 +4,21 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+// Create
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// Detail
 router.get('/todos/:id', (req, res) => {
   const UserId = req.user.id
   const id = req.params.id
