8f0ef0574cd2267b838314deb010e0bb88888d0e
chiangjou <tiffanyj.chiang@gmail.com>
2023-07-22 15:33:57 +0800

    refactor express router

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
 const session = require('express-session')
 
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
-
+const routes = require('./routes')
 const usePassport = require('./config/passport')
-const passport = require('passport')
 
 const app = express()
 const PORT = 3000
@@ -28,72 +23,7 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
-// 設定路由
-// 首頁
-app.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos: todos })})
-    .catch((error) => { return res.status(422).json(error)})
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
-})
-
-// 認證系統的路由
-// 登入頁面
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-// 登入檢查
-app.post('/users/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
-
-// 註冊頁面
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-// 註冊檢查
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.findOne({ where: { email } }).then(user => {
-    if (user) {
-      console.log('User already exists.')
-      return res.render('register', {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    }
-    return bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(password, salt))
-      .then(hash => User.create({
-        name,
-        email,
-        password: hash
-      }))
-      .then(() => res.redirect('/'))
-      .catch(error => console.log(error))
-  })
-    .then(user => res.redirect('/'))
-})
-
-// 登出
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is listening on http://localhost:${PORT}`)
